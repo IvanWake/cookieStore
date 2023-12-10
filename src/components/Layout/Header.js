9ca4a63b549e0e8838a494a82ecc19f55c03157d
Sergey Andrianov <sergeygggtop@gmail.com>
2023-12-10 22:36:42 +0300

    Add lyaout for header, change file system, implement logic for router

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,32 +1,40 @@
 import React, { Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faMagnifyingGlass, 
+    faSliders,
+    faCartShopping
+ } from '@fortawesome/free-solid-svg-icons';
+
 import logo from '../../assets/logo.svg';
+import { header } from '../../store/styles.js';
+
 const Header = () => {
     return (
         <Fragment>
-            <header>
-                <div className="header">
-                    <div>
+            <header className={header.header}>
+                <div className={header.headerContainer}>
+                    <div className={header.headerDiv}>
                         <img src={logo} alt="CookieStore" width="32" />
-                            <div className="hr"></div>
-                            <div className="search">
-                                <i className="fa-solid fa-magnifying-glass"></i>
-                                <input type="text" placeholder="Search..."/>
-                            </div>
-                            <button className="orange-btn"><i className="fa-solid fa-sliders"></i> Filter</button>
+                        <div className={header.hr}></div>
+                        <div className={header.search}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className={header.searchI} />
+                            <input type="text" placeholder="Search..." className={header.searchInput} />
+                        </div>
+                        <button className={header.button}><FontAwesomeIcon icon={faSliders}/> Filter</button>
                     </div>
 
-                    <div>
-                        <button className="orange-btn" id="cart"><i className="fa-solid fa-cart-shopping"></i> Cart
-                        </button>
-                        <div className="profile">
-                            <div className="hr"></div>
-                            <img className="profile-image" src="https://avatars.githubusercontent.com/u/45458770?v=4"
-                                 height="32" alt="Хуй"/>
-                            <div className="profile-wrapper">
+                    <div className={header.headerDiv}>
+                        <button className={header.button} id="cart"><FontAwesomeIcon icon={faCartShopping}/> Cart</button>
+                        <div className={header.profile}>
+                            <div className={header.hr}></div>
+                            <img className={header.profileImage} src="https://avatars.githubusercontent.com/u/45458770?v=4"
+                                width="32" alt="profile" />
+                            <div className={header.profileWrapper}>
                                 <div className="profile-info">
                                     <img className="profile-image"
-                                         src="https://avatars.githubusercontent.com/u/45458770?v=4"
-                                         height="32" alt="хуй"/>
+                                        src="https://avatars.githubusercontent.com/u/45458770?v=4"
+                                        width="32" alt="profile" />
                                     <div className="info">
                                         <div className="name">Cookie Shop</div>
                                         <div className="email">cookieShop@mail.ru</div>
---
+++ src/store/styles.js
@@ -0,0 +1,15 @@
+export const header = {
+    header: 'z-150 flex fixed top-0 left-0 right-0 bg-[#ffcca7]',
+    headerContainer: 'flex items-center justify-between mx-auto p-4 w-full max-w-1400',
+    headerDiv: 'flex items-center gap-4',
+    hr: 'w-px h-[20px] bg-[#ff9f5a]',
+    headerInput: 'flex items-center justify-center max-h-32',
+    search: 'flex items-center relative p-2 pl-8 rounded-lg bg-white',
+    searchI: 'text-[#00000040] absolute left-2',
+    searchInput: 'flex items-center justify-center max-h-32 bg-none border-none font-medium',
+    button: 'p-2 px-4 rounded-lg bg-[#ff9f5a] text-white border-none transition ease-in duration-150 active:scale-95 hover:bg-[#ff9f5a80]',
+    profile: 'relative flex items-center gap-4',
+    profileImage: 'cursor-pointer rounded-full',
+    profileWrapper: 'z-100 hidden absolute top-12 right-0 h-auto w-auto min-w-300 bg-[#ffcca7] shadow-md rounded-bl-lg rounded-tr-lg pb-4',
+};
+
