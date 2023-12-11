b709f95fef1164e056c89157234b8433b3388609
Ivan Wake <purplebaby16@vk.com>
2023-12-11 15:27:27 +0300

    Add Layout Main and styles for Product and List

---
+++ src/components/Layout/Main.js
@@ -0,0 +1,10 @@
+import { main } from '../../store/styles';
+const Main = (props) => {
+    return (
+        <main className={main.main}>
+            {props.children}
+        </main>
+    );
+}
+
+export default Main;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from "react";
 import Header from "../components/Layout/Header";
+import Main from "../components/Layout/Main";
+import ProductList from "../components/Product/ProductList";
+
 const Home = () => {
     return (
         <Fragment>
             <Header/>
+            <Main>
+                <ProductList />
+            </Main>
         </Fragment>
 
     );
