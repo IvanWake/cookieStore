a90786e18e477d17f7d60064a8aa028e2f3c8b32
Sergey Andrianov <sergeygggtop@gmail.com>
2023-12-11 10:20:38 +0300

    Added styles for ProfileWrapper

--- src/components/Layout/ProfileWrapper.js
+++ src/components/Layout/ProfileWrapper.js
@@ -11,20 +11,20 @@ import {
 const ProfileWrapper = () => {
     return (
         <div className={profile.profileWrapper}>
-            <div className="profile-info">
-                <img className="profile-image"
-                     src="https://avatars.githubusercontent.com/u/45458770?v=4"
-                     width="32" alt="profile" />
-                <div className="info">
-                    <div className="name">Cookie Shop</div>
-                    <div className="email">cookieShop@mail.ru</div>
+            <div className={profile.profileInfo}>
+                <img className={profile.profileImage}
+                    src="https://avatars.githubusercontent.com/u/45458770?v=4"
+                    width="32" alt="profile" />
+                <div className={profile.email}>
+                    <div>Cookie Shop</div>
+                    <div className={profile.email}>cookieShop@mail.ru</div>
                 </div>
             </div>
-            <div className="link"><FontAwesomeIcon icon={faListUl} /> My orders</div>
-            <div className="link"><FontAwesomeIcon icon={faMapLocationDot} /> My addresses
+            <div className={profile.link}><FontAwesomeIcon icon={faListUl} className={profile.i}/> My orders</div>
+            <div className={profile.link}><FontAwesomeIcon icon={faMapLocationDot} className={profile.i}/> My addresses
             </div>
-            <div className="link"><FontAwesomeIcon icon={faGear} /> Settings</div>
-            <div className="link"><FontAwesomeIcon icon={faArrowRightFromBracket} /> Log out
+            <div className={profile.link}><FontAwesomeIcon icon={faGear} className={profile.i}/> Settings</div>
+            <div className={profile.link}><FontAwesomeIcon icon={faArrowRightFromBracket} className={profile.i}/> Log out
             </div>
         </div>
     );
--- src/store/styles.js
+++ src/store/styles.js
@@ -1,18 +1,23 @@
 export const header = {
     header: 'z-150 flex fixed top-0 left-0 right-0 bg-[#ffcca7]',
-    headerContainer: 'flex items-center justify-between mx-auto p-4 w-full max-w-1400',
-    headerDiv: 'flex items-center gap-4',
+    headerContainer: 'flex items-center justify-between mx-auto p-4 w-full max-w-[1400px]',
+    headerDiv: 'flex items-center gap-4 max-h-[32px] ',
     hr: 'w-px h-[20px] bg-[#ff9f5a]',
-    headerInput: 'flex items-center justify-center max-h-32',
-    search: 'flex items-center relative p-2 pl-8 rounded-lg bg-white',
-    searchI: 'text-[#00000040] absolute left-2',
-    searchInput: 'flex items-center justify-center max-h-32 bg-none border-none font-medium',
-    button: 'p-2 px-4 rounded-lg bg-[#ff9f5a] text-white border-none transition ease-in duration-150 active:scale-95 hover:bg-[#ff9f5a80]',
+    search: 'flex items-center relative py-2 px-4 pl-8 rounded-[0.6rem] bg-white',
+    searchI: 'text-black/40 absolute left-2',
+    searchInput: 'text-sm bg-none border-none focus:outline-none',
+    button: 'flex items-center gap-2 py-2 px-4 rounded-[0.6rem] bg-[#ff9f5a] max-h-[32px] text-white border-none transition ease-in duration-150 active:scale-95 hover:bg-[#ff9f5a80]',
     profile: 'relative flex items-center gap-4',
     profileImage: 'cursor-pointer rounded-full',
 };
 
 export const profile = {
-    profileWrapper: 'z-100 absolute top-12 right-0 h-auto w-auto min-w-300 bg-[#ffcca7] shadow-md rounded-bl-lg rounded-tr-lg pb-4',
+    profileWrapper: 'z-100 absolute top-12 right-0 h-auto w-auto min-w-[300px] bg-[#ffcca7] shadow-md rounded-b-[0.8rem] pb-4',
+    profileInfo: 'flex items-center rounded-md gap-2 p-4 m-2 bg-black/10 hover:bg-black/15',
+    profileImage: 'cursor-pointer rounded-full',
+    info: 'flex flex-col',
+    email: 'text-black/70',
+    link: 'flex justify-start items-center gap-4 p-2 px-4 m-0 transition-all duration-300 ease-in-out hover:bg-black/10',
+    i: 'w-4 text-black/60'
 }
 
