19dc27b9e61a1df4e6fb6e5c8a1aa1c4bb5cddac
Ivan Wake <purplebaby16@vk.com>
2023-12-11 19:48:10 +0300

    Change styles(-webkit's)

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ import error from "../assets/404.png";
 const NotFound = () => {
     return (
         <div className={notFound.div}>
-            <img src={error} alt="404" width="400px" />
+            <img src={error} className='pointer-events-none' alt="404" width="400px" />
             <h1 className={notFound.h1}>Oops! Page not found</h1>
         </div>
     );
