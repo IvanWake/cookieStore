6a9e192aa74e4b523f93f50c6b971d3d6e658a7a
Ivan Wake <purplebaby16@vk.com>
2023-12-12 20:12:22 +0300

    Change logo

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { products } from "../../store/styles";
 import Product from "./Product";
 const ProductList = () => {
     return (
-        <Fragment>
-            <Product />
-        </Fragment>
+        <div className={products.productList}>
+            <Product key={'key1'}/>
+        </div>
     );
 }
 
--- src/store/styles.js
+++ src/store/styles.js
@@ -47,7 +47,7 @@ export const products = {
 
 export const category = {
     categoryName: 'font-extrabold text-[2.5rem]',
-    categoryDiv: 'flex sticky top-[4rem] items-center gap-4 bg-[#FFE4D0] pt-4 pb-2',
+    categoryDiv: 'flex sticky top-[4rem] items-center gap-4 bg-[#FFE4D0] pt-4 pb-2 cursor-pointer',
     category: 'flex justify-center items-center w-[8.25rem] h-8 rounded-[0.8rem] border-2 border-black transition-all duration-300 ease-in-out font-bold hover:text-black hover:bg-white hover:border-white hover:border-2 active:scale-95',
     categorySelected: 'text-black bg-white border-2 border-white'
 }
