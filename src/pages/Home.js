6a1ab7aca251052ed5d21c7633aa7ced80b551b0
Sergey Andrianov <sergeygggtop@gmail.com>
2023-12-12 19:34:54 +0300

    Implement logic for categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "../components/Layout/Header";
+import Categories from "../components/Categories/Categories";
+
 const Home = () => {
     return (
         <Fragment>
             <Header/>
+            <Categories/>
         </Fragment>
-
     );
 }
 
--- src/store/styles.js
+++ src/store/styles.js
@@ -25,3 +25,10 @@ export const notFound = {
     div: 'flex flex-col justify-center items-center absolute inset-x-0 inset-y-0 gap-0',
     h1: 'font-medium text-[6em]'
 };
+
+export const category = {
+    categoryName: 'font-extrabold text-[2.5rem]',
+    categoryDiv: 'flex sticky top-[4rem] items-center gap-4 bg-[#FFE4D0] pt-4 pb-2',
+    category: 'flex justify-center items-center w-[8.25rem] h-8 rounded-[0.8rem] border-2 border-black transition-all duration-300 ease-in-out font-bold hover:text-black hover:bg-white hover:border-white hover:border-2 active:scale-95',
+    categorySelected: 'text-black bg-white border-2 border-white'
+}
