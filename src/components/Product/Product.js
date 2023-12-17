2a49941d176c211bd54bf59b349ee0796aeb907e
Ivan Wake <purplebaby16@vk.com>
2023-12-17 16:09:41 +0300

    Added counter(Product)

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,27 +1,46 @@
+import { useState } from 'react';
 import { products } from "../../store/styles";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import product from "../../assets/american.png";
 
 const Product = (props) => {
+    const [productCounter, setProductCounter] = useState(1);
+
+    const increaseCounterHandler = () => {
+        if (productCounter === 10) {
+            return;
+        }
+        setProductCounter(prevState => prevState + 1);
+    }
+
+    const decreaseCounterHandler = () => {
+        if (productCounter === 1) {
+            return;
+        }
+        setProductCounter(prevState => prevState - 1);
+    }
+
     return (
         <div className={products.product}>
             <div className={products.productCol}>
                 <img src={product} alt="productImage" className="product-image" />
                     <div className={products.productCount}>
-                        <button className={products.editCount}><FontAwesomeIcon icon={faMinus}/></button>
-                        <div className="counter">1</div>
-                        <button className={products.editCount}><FontAwesomeIcon icon={faPlus}/></button>
+                        <button className={products.editCount} onClick={decreaseCounterHandler} ><FontAwesomeIcon icon={faMinus}/></button>
+                        <div className="counter">{productCounter}</div>
+                        <button className={products.editCount} onClick={increaseCounterHandler} ><FontAwesomeIcon icon={faPlus}/></button>
                     </div>
             </div>
             <div className={products.productCol}>
                 <div className={products.productDesc}>
                     <div className={products.productDescHeader}>
                         <div className={products.productName}>{props.name}</div>
-                        <div className={products.productPrice}>$ {props.price}</div>
+                        <div className={products.productPrice}>$ 0.98</div>
                     </div>
                     <div className={products.productDescText}>
-                        {props.description}
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Amet mauris commodo quis imperdiet massa
+                        tincidunt. Sed id semper risus in hendrerit.
                     </div>
                 </div>
 
